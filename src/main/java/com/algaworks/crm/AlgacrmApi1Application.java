66adc694124b226ffda9d059d2c42470bf83c714
Ricardo Tavares da Silva <ricardotavaresjc@gmail.com>
2021-08-23 09:37:31 -0300

    Primeiro Commito

---
+++ src/main/java/com/algaworks/crm/AlgacrmApi1Application.java
@@ -0,0 +1,13 @@
+package com.algaworks.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgacrmApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgacrmApi1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/algaworks/crm/model/Cliente.java
@@ -0,0 +1,28 @@
+package com.algaworks.crm.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+
+@Entity
+@Data
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+			
+
+}
